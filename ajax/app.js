6d154315dcb8708567f383dfd4af7647b8f8befa
Matthew Edmond <matthewcedmond@gmail.com>
2021-03-14 09:31:36 -0500

    Updates

--- ajax/app.js
+++ ajax/app.js
@@ -24,3 +24,33 @@ function loadCustomer(e) {
 
     xhr.send();
 }
+
+document.getElementById('button2').addEventListener('click', loadCustomers);
+
+function loadCustomers(e) {
+    const xhr = new XMLHttpRequest('GET');
+
+    xhr.open('GET', 'customers.json', true);
+
+    xhr.onload = function(){
+        if(this.status === 200) {
+            const customers = JSON.parse(this.responseText);
+            let output = '';
+
+            customers.forEach(function(customer) {
+                output += `
+                <ul>
+                    <li>ID ${customer.id}</li>
+                    <li>Name ${customer.name}</li>
+                    <li>Company ${customer.company}</li>
+                    <li>Phone ${customer.phone}</li>
+                </ul>
+                `
+            });
+
+            document.getElementById('customers').innerHTML = output;
+        }
+    }
+
+    xhr.send();
+}
